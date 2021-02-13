cb3283372e300b325a3e81002396ef5cf9a8ca48
Sam Westerman <sam.westerman@usaepay.com>
2021-02-12 23:47:56 -0800

    added no-sybols, whee

---
+++ fizzbuzz/no-symbols.rb
@@ -0,0 +1,16 @@
+class Integer alias each times end or def Fizz samp
+__method__ end and class Float false end or def thr
+__LINE__ end and begin alias tr p end or def Buzz l
+__method__ end and tap do Fizz nil end and def five
+__LINE__ end and class Integer def FizzBuzz _ersand
+__method__ end and for z in sqrt object_id do break
+end or define_method String z do z end and def modt
+send String modulo thr rescue nil end and def modfv
+send String modulo five rescue nil end and def to_a
+Hash succ and thr and Array nil end and def to_hash
+if modt and modfv then puts FizzBuzz nil elsif modt
+then puts Fizz self elsif modfv then puts Buzz self
+else puts String self end and false or Hash nil end
+end and class Struct p if new String Hash do __id__
+end and new String Float do for num in object_id do
+Array num end end end and at_exit do print ruby end
